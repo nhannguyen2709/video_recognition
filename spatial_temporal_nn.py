ca29b2f03db339a1d0c377d6b197d9596f2dfdd7
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-04-05 23:57:20 +0700

    Add a Callback object to evaluate accuracy at video level

--- spatial_temporal_nn.py
+++ spatial_temporal_nn.py
@@ -1,14 +1,19 @@
-import argparse, os
+import argparse, os, pickle
+
+import numpy as np
+from cv2 import imread, resize
 
 import tensorflow as tf
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
-
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, Callback
 from keras.optimizers import Adam
 from keras.backend import tensorflow_backend as K
-from dataloader.keras_data import VideosFrames
 from keras_models import VGG19_SpatialTemporalGRU
+from keras.utils import to_categorical
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.utils import resample
 
+from dataloader.keras_data import VideosFrames
 # limit tensorflow's memory usage
 # import tensorflow as tf
 # config = tf.ConfigProto()
@@ -33,6 +38,57 @@ parser.add_argument('--num-workers', default=4,
 parser.add_argument('--initial-epoch', default=0, 
     type=int, metavar='N', help='manual epoch number (useful on restarts)')
 
+
+class VideoLevelEvaluation(Callback):
+    def __init__(self, data_path, frame_counts_path, 
+                 validation_data, interval=10, num_videos_eval=4, 
+                 num_frames_sampled=32, num_segments=10, num_classes=7):
+        super(Callback, self).__init__()
+        
+        frame_counts = pickle.load(open(frame_counts_path, 'rb'))
+        video_filenames = validation_data.video_filenames
+        self.video_paths = [os.path.join(data_path, filename) for filename in video_filenames] 
+        self.y_val = np.array(validation_data.y)
+        self.valid_video_frame_counts = [frame_counts[filename]
+                                         for filename in video_filenames]
+        self.interval = interval
+        self.num_videos_eval = num_videos_eval
+        self.num_frames_sampled = num_frames_sampled
+        self.num_segments = num_segments
+        self.num_classes = num_classes
+
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch % self.interval == 0:
+            # sample some videos to evaluate from the validation set
+            eval_video_paths, eval_y_val, eval_video_frame_counts = resample(self.video_paths, self.y_val, self.valid_video_frame_counts, 
+                                                                             replace=False, n_samples=self.num_videos_eval)             
+            avg_eval_video_preds = np.zeros(shape=(self.num_videos_eval, self.num_classes))
+
+            for _ in range(self.num_segments):
+                eval_video_frames = []
+                for video_path, frame_counts in zip(eval_video_paths, eval_video_frame_counts):
+                    frames_snippet = self.sample_frames_snippet(video_path, frame_counts)
+                    single_video_frames = np.array([resize(imread(os.path.join(video_path, frame)), (224, 224))
+                                                    for frame in frames_snippet])
+                    eval_video_frames.append(single_video_frames)
+                eval_video_frames = np.array(eval_video_frames)
+                eval_video_preds = self.model.predict(eval_video_frames, verbose=0)
+                avg_eval_video_preds += eval_video_preds/self.num_segments # average the probabilities across segments
+            
+            eval_loss = log_loss(y_true=to_categorical(eval_y_val, num_classes=self.num_classes), 
+                                 y_pred=avg_eval_video_preds)
+            eval_acc = accuracy_score(y_true=eval_y_val,
+                                      y_pred=np.argmax(avg_eval_video_preds, axis=1))
+            print("\n Epoch: {:d} - eval. loss: {:.4f} - eval. acc.: {:.4f}".format(epoch+1, eval_loss, eval_acc))
+
+    def sample_frames_snippet(self, video_path, frame_counts):
+        frames = np.array([filename for filename in sorted(os.listdir(video_path)) if filename.endswith('.jpg')])
+        start_frameidx_in_snippet = np.random.randint(0, frame_counts - self.num_frames_sampled)
+        end_frameidx__in_snippet = start_frameidx_in_snippet + self.num_frames_sampled
+        framesidx_snippet = range(start_frameidx_in_snippet, end_frameidx__in_snippet)
+        return frames[framesidx_snippet]
+
+
 def train():
     global args
     args = parser.parse_args()
@@ -42,7 +98,7 @@ def train():
                                        frame_counts_path='dataloader/dic/merged_frame_count.pickle',
                                        batch_size=args.batch_size, num_frames_sampled=args.num_frames_sampled)
     
-    validation_videos_frames = VideosFrames(data_path='data/NewVideos/validation_videos',
+    valid_videos_frames = VideosFrames(data_path='data/NewVideos/validation_videos',
                                             frame_counts_path='dataloader/dic/merged_frame_count.pickle',
                                             batch_size=args.batch_size, num_frames_sampled=args.num_frames_sampled,
                                             shuffle=False)
@@ -66,10 +122,13 @@ def train():
     save_best = ModelCheckpoint('checkpoint/spatial_temporal/weights.best.hdf5',
                                 monitor='train_loss', verbose=1, 
                                 save_best_only=True, mode='max')
-    callbacks = [checkpoint, save_best, reduce_lr]
+    video_level_eval = VideoLevelEvaluation(validation_data=valid_videos_frames, 
+                                            data_path='data/NewVideos/validation_videos', 
+                                            frame_counts_path='dataloader/dic/merged_frame_count.pickle')
+    callbacks = [checkpoint, reduce_lr, save_best, video_level_eval]
 
     model.fit_generator(generator=train_videos_frames, epochs=args.epochs, 
-                        callbacks=callbacks, validation_data=validation_videos_frames,
+                        callbacks=callbacks, validation_data=valid_videos_frames,
                         workers=args.num_workers, initial_epoch=args.initial_epoch)
 
 
@@ -78,7 +137,7 @@ def train_with_finetune():
                                        frame_counts_path='dataloader/dic/merged_frame_count.pickle',
                                        batch_size=args.batch_size, num_frames_sampled=args.num_frames_sampled)
     
-    validation_videos_frames = VideosFrames(data_path='data/NewVideos/validation_videos',
+    valid_videos_frames = VideosFrames(data_path='data/NewVideos/validation_videos',
                                             frame_counts_path='dataloader/dic/merged_frame_count.pickle',
                                             batch_size=args.batch_size, num_frames_sampled=args.num_frames_sampled,
                                             shuffle=False)
@@ -102,10 +161,13 @@ def train_with_finetune():
     save_best = ModelCheckpoint('checkpoint/spatial_temporal/weights.best.hdf5',
                                 monitor='train_loss', verbose=1, 
                                 save_best_only=True, mode='max')
-    callbacks = [checkpoint, save_best, reduce_lr]
+    video_level_eval = VideoLevelEvaluation(validation_data=valid_videos_frames,
+                                            data_path='data/NewVideos/validation_videos', 
+                                            frame_counts_path='dataloader/dic/merged_frame_count.pickle')
+    callbacks = [checkpoint, reduce_lr, save_best, video_level_eval]
 
     model.fit_generator(generator=train_videos_frames, epochs=args.epochs, 
-                        callbacks=callbacks, validation_data=validation_videos_frames,
+                        callbacks=callbacks, validation_data=valid_videos_frames,
                         workers=args.num_workers, initial_epoch=args.initial_epoch)
 
 
