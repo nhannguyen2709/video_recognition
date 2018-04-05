535ee29da1f892c438c26f63d8fbbe94af4d066f
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-04-05 09:32:17 +0700

    Minor update

--- keras_models.py
+++ keras_models.py
@@ -1,15 +1,15 @@
 from keras.models import Model, Sequential
-from keras.layers import Dense, Input, Flatten, Conv2D, MaxPooling2D, GlobalMaxPooling2D, GRU, TimeDistributed, Concatenate
+from keras.layers import Dense, Input, Flatten, Conv2D, MaxPooling2D, GlobalMaxPooling2D, GRU, TimeDistributed, Bidirectional, Concatenate
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg19 import VGG19
 
 
 def MotionTemporalGRU(poses_input_shape, classes):
     poses = Input(shape=poses_input_shape, name='poses')
-    poses_features = GRU(256, return_sequences=True, recurrent_dropout=0.2,
-                          dropout=0.2, name='gru1')(poses)
-    poses_features = GRU(128, recurrent_dropout=0.2,
-                          name='gru2')(poses_features)
+    poses_features = Bidirectional(GRU(256, return_sequences=True, recurrent_dropout=0.2,
+                                       dropout=0.2, name='gru1'))(poses)
+    poses_features = Bidirectional(GRU(256, recurrent_dropout=0.2,
+                                       name='gru2'))(poses_features)
     outputs = Dense(classes, activation='softmax',
                     name='predictions')(poses_features)
     model = Model(inputs=poses, outputs=outputs)
@@ -17,7 +17,7 @@ def MotionTemporalGRU(poses_input_shape, classes):
     return model
 
 
-def VGG16_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers):
+def VGG16_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers=False):
     frames = Input(shape=frames_input_shape, name='frames')
     # Block 1
     frames_features = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1'))(frames)
@@ -47,9 +47,10 @@ def VGG16_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers):
     frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))(frames_features)
     frames_features = TimeDistributed(GlobalMaxPooling2D())(frames_features)  
     
-    frames_features = GRU(256, return_sequences=True, recurrent_dropout=0.2,
+    frames_features = GRU(512, return_sequences=True, recurrent_dropout=0.2,
                           dropout=0.2, name='gru1')(frames_features)
-    frames_features = GRU(128, recurrent_dropout=0.2,
+    frames_features = GRU(512, 
+                          recurrent_dropout=0.2,
                           name='gru2')(frames_features)
     outputs = Dense(classes, activation='softmax',
                     name='predictions')(frames_features)
@@ -70,7 +71,7 @@ def VGG16_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers):
     return model
 
 
-def VGG19_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers):
+def VGG19_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers=False):
     frames = Input(shape=frames_input_shape, name='frames')
     # Block 1
     frames_features = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1'))(frames)
@@ -103,9 +104,10 @@ def VGG19_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers):
     frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4'))(frames_features)
     frames_features = TimeDistributed(GlobalMaxPooling2D())(frames_features)  
     
-    frames_features = GRU(256, return_sequences=True, recurrent_dropout=0.2,
+    frames_features = GRU(512, return_sequences=True, recurrent_dropout=0.2,
                           dropout=0.2, name='gru1')(frames_features)
-    frames_features = GRU(128, recurrent_dropout=0.2,
+    frames_features = GRU(512, 
+                          recurrent_dropout=0.2,
                           name='gru2')(frames_features)
     outputs = Dense(classes, activation='softmax', name='predictions')(frames_features)
     model = Model(inputs=frames, outputs=outputs)  
@@ -124,5 +126,64 @@ def VGG19_SpatialTemporalGRU(frames_input_shape, classes, finetune_conv_layers):
     return model
 
 
+def VGG19_SpatialMotionTemporalGRU(frames_input_shape, poses_input_shape, classes, finetune_conv_layers=False):
+    frames = Input(shape=frames_input_shape, name='frames')
+    poses = Input(shape=poses_input_shape, name='poses')
+    # Block 1
+    frames_features = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1'))(frames)
+    frames_features = TimeDistributed(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))(frames_features)
+    frames_features = TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))(frames_features)   
+    
+    # Block 2
+    frames_features = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))(frames_features)
+    frames_features = TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))(frames_features)  
+    
+    # Block 3
+    frames_features = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4'))(frames_features)
+    frames_features = TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))(frames_features)   
+   
+    # Block 4
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4'))(frames_features)
+    frames_features = TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))(frames_features)   
+    
+    # Block 5
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))(frames_features)
+    frames_features = TimeDistributed(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4'))(frames_features)
+    frames_features = TimeDistributed(GlobalMaxPooling2D())(frames_features)  
+    
+    merged_features = Concatenate(name='concatenate')([frames_features, poses])
+    merged_features = GRU(512, return_sequences=True, recurrent_dropout=0.2,
+                          dropout=0.2, name='gru1')(merged_features)
+    merged_features = GRU(512, 
+                          recurrent_dropout=0.2,
+                          name='gru2')(merged_features)
+    outputs = Dense(classes, activation='softmax', name='predictions')(merged_features)
+    model = Model(inputs=[frames, poses], outputs=outputs)  
+
+    # Overload model's weights with the pre-trained ImageNet weights of VGG19
+    vgg19 = VGG19(include_top=False, input_shape=frames_input_shape[1:])
+    for i, layer in enumerate(vgg19.layers[:-1]):
+        model.layers[i].set_weights(weights=layer.get_weights())
+        model.layers[i].trainable = False  
+
+    # Finetune the last 2 convolutional layers in block 5 of VGG19
+    if finetune_conv_layers == True:
+        model.layers[-5].trainable = True
+        model.layers[-6].trainable = True
+
+    return model
+
+
 if __name__=='__main__':
+    model = VGG19_SpatialMotionTemporalGRU((32, 224, 224, 3), (32, 54), classes=7)
+    model.summary()
     pass
