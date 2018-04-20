82c9ac7f22ed8566094f93932b3b314d2e7bbf34
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-04-20 13:28:54 +0700

    Add __len__() to the PennAction Sequence object

--- dataloader/keras_data.py
+++ dataloader/keras_data.py
@@ -3,13 +3,14 @@ import os
 import numpy as np
 import pickle
 import random
+from scipy import io
 
 from cv2 import resize, imread
 from sklearn.utils import shuffle
 from keras.utils import Sequence, to_categorical
 
 
-class UCF101VideosFrames(Sequence):
+class UCF101(Sequence):
     def __init__(self, data_path, frame_counts_path, batch_size,
                  num_frames_sampled, num_classes, shuffle=True):
         self.data_path = data_path
@@ -103,6 +104,7 @@ class PennAction(Sequence):
         self.labels_path = labels_path
         self.get_videos_paths()
         self.extract_mat_file()
+        print('Found {} videos belonging to {} classes'.format(len(self.x), len(self.labels)))
         self.batch_size = batch_size
         self.num_frames_sampled = num_frames_sampled
         self.num_classes = num_classes
@@ -127,10 +129,10 @@ class PennAction(Sequence):
             mat = io.loadmat(os.path.join(self.labels_path, mat_file))
             frame_counts[i] = mat['nframes'][0][0]
             y[i] = mat['action'][0]
-
+            
         self.frame_counts = frame_counts
         self.labels = sorted(set(y))
-        self.y = np.vectorize(lambda x: labels.index(x))(
+        self.y = np.vectorize(lambda x: self.labels.index(x))(
             y)  # convert label into class
 
     def sample_frames(self, video_path, frame_count):
@@ -138,18 +140,27 @@ class PennAction(Sequence):
             os.listdir(video_path)) if filename.endswith('.jpg')])
         sampled_frames_idx = sorted(np.random.choice(
             frame_count, size=int(self.num_frames_sampled), replace=False))
-        return all_frames[sampled_frames_idx]
+        return sampled_frames_idx, all_frames[sampled_frames_idx]
+    
+    def __len__(self):
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
 
     def __getitem__(self, idx):
         batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+        
+        batch_mat_files = [video_path.replace('frames/', 'labels/') for video_path in batch_x]
         batch_frame_counts = self.frame_counts[idx * \
             self.batch_size:(idx + 1) * self.batch_size]
         batch_video_frames = np.zeros(
             (self.batch_size, self.num_frames_sampled, 224, 224, 3))
+        batch_video_poses = np.zeros(
+            (self.batch_size, self.num_frames_sampled, 26))
+
         for i, video_path in enumerate(batch_x):
-            sampled_frames = self.sample_frames(
+            sampled_frames_idx, sampled_frames = self.sample_frames(
                 video_path, batch_frame_counts[i])
+            # extract video frames
             batch_video_frames[i] = [
                 resize(
                     imread(
@@ -158,8 +169,28 @@ class PennAction(Sequence):
                             frame)),
                     (224,
                      224)) for frame in sampled_frames]
+            # extract poses from sampled frames
+            first_frame = imread(os.path.join(video_path, sampled_frames[0]))
+            x_max, y_max = first_frame.shape[:2]
+            mat = io.loadmat(batch_mat_files[i])
+            # normalize poses coordinates
+            poses_x, poses_y = mat['x'] / x_max, mat['y'] / y_max
+            poses_x_y = np.hstack([poses_x, poses_y])
+            poses_x_y = poses_x_y[sampled_frames_idx]
+            batch_video_poses[i] = poses_x_y
 
         batch_video_frames = batch_video_frames / 255.
 
-        return batch_video_frames, to_categorical(
+        return [batch_video_frames, batch_video_poses], to_categorical(
             np.array(batch_y), num_classes=self.num_classes)
+
+
+if __name__=='__main__':
+    penn_action = PennAction(frames_path='data/Penn_Action/frames', 
+                             labels_path='data/Penn_Action/labels',
+                             batch_size=8, num_frames_sampled=16, num_classes=15,
+                             shuffle=False)
+
+    x, y = penn_action.__getitem__(0)
+    print(x[0].shape, x[1].shape)
+    
