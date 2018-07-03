dae7ac487b8a23f968acfdd15da4b164ee9596a9
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-07-03 17:26:04 +0700

    Add more arguments eg base conv. net, num segments to divide a video, input shape

--- train_tsn_spatial_stream.py
+++ train_tsn_spatial_stream.py
@@ -1,8 +1,6 @@
 import argparse
 import numpy as np
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 from keras.backend import tensorflow_backend as K
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint, ReduceLROnPlateau
@@ -10,40 +8,31 @@ from keras.models import load_model
 from keras.optimizers import SGD
 
 from dataloader.keras_data import UCF101Frames
-from keras_models import TemporalSegmentNetworks_SpatialStream
+from keras_models import TSNs_SpatialStream
 
-parser = argparse.ArgumentParser(
-    description='Training the Temporal Segment Networks on UCF101 data')
-parser.add_argument(
-    '--filepath',
-    default='checkpoint/ucf101_spatial_stream.hdf5',
-    type=str,
-    metavar='PATH',
-    help="path to checkpoint best model's state and weights")
-parser.add_argument(
-    '--epochs',
-    default=20,
-    type=int,
-    metavar='N',
-    help='number of total epochs')
-parser.add_argument(
-    '--batch-size',
-    default=8,
-    type=int,
-    metavar='N',
-    help='number of videos in a single mini-batch')
-parser.add_argument(
-    '--train-lr',
-    default=0.001,
-    type=float,
-    metavar='LR',
-    help='learning rate initialized')
-parser.add_argument(
-    '--num-workers',
-    default=12,
-    type=int,
-    metavar='N',
-    help='maximum number of processes to spin up')
+parser = argparse.ArgumentParser(description="Training the TSNs' spatial stream on UCF101 data")
+parser.add_argument('--arch', type=str, default="Xception")
+parser.add_argument('--num-segments', type=int, default=3)
+parser.add_argument('--input-shape', nargs='+', type=int)
+parser.add_argument('--consensus-type', '--cons', type=str, default='avg',
+                    choices=['avg', 'max', 'attention'])
+
+parser.add_argument('--train-path', type=str, metavar='PATH',
+                    default='data/UCF101/train/frames/')
+parser.add_argument('--val-path', type=str, metavar='PATH',
+                    default='data/UCF101/validation/frames/')
+parser.add_argument('--filepath', type=str, metavar='PATH',
+                    default='checkpoint/ucf101_spatial_stream.hdf5',
+                    help="path to checkpoint best model's state and weights")
+
+parser.add_argument('--epochs', default=20, type=int, metavar='N',
+                    help='number of total epochs')
+parser.add_argument('--batch-size', default=8, type=int, metavar='N',
+                    help='number of videos in a single mini-batch')
+parser.add_argument('--train-lr', default=0.001, type=float, metavar='LR',
+                    help='learning rate initialized')
+parser.add_argument('--num-workers', default=12, type=int, metavar='N',
+                    help='number of data loading workers')
 
 
 def schedule(epoch, lr):
@@ -59,11 +48,15 @@ def train():
     print(args)
 
     train_videos = UCF101Frames(
-        frames_path='data/UCF101/train/frames/',
-        batch_size=args.batch_size)
-    valid_videos = UCF101Frames(
-        frames_path='data/UCF101/validation/frames',
+        frames_path=args.train_path,
         batch_size=args.batch_size,
+        input_shape=tuple(args.input_shape),
+        num_segments=args.num_segments)
+    valid_videos = UCF101Frames(
+        frames_path=args.val_path,
+        batch_size=args.batch_size,
+        input_shape=tuple(args.input_shape),
+        num_segments=args.num_segments,
         shuffle=False)
 
     reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=np.sqrt(0.1), min_lr=1e-6,
@@ -80,9 +73,12 @@ def train():
     if os.path.exists(args.filepath):
         model = load_model(args.filepath)
     else:
-        model = TemporalSegmentNetworks_SpatialStream(
-            input_shape=(299, 299, 3), dropout_prob=0.8,
-            classes=len(train_videos.labels))
+        model = TSNs_SpatialStream(
+            input_shape=args.input_shape,
+            classes=len(train_videos.labels),
+            num_segments=args.num_segments,
+            base_model=args.arch,
+            consensus_type=args.consensus_type)
         model.compile(optimizer=SGD(lr=args.train_lr, momentum=0.9),
                       loss='categorical_crossentropy',
                       metrics=['acc'])
@@ -95,4 +91,6 @@ def train():
 
 
 if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
     train()
