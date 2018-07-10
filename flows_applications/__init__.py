fd5be6d837a2e0c2f4ee59d1cd97d2a09ffce543
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2018-07-10 17:38:33 +0700

    Init file

---
+++ flows_applications/__init__.py
@@ -0,0 +1,9 @@
+# from flows_applications.vgg16 import VGG16
+# from flows_applications.vgg19 import VGG19
+from flows_applications.resnet50 import ResNet50
+# from flows_applications.inception_v3 import InceptionV3
+# from flows_applications.inception_resnet_v2 import InceptionResNetV2
+from flows_applications.xception import Xception
+# from flows_applications.mobilenet import MobileNet
+# from flows_applications.densenet import DenseNet121, DenseNet169, DenseNet201
+# from flows_applications.nasnet import NASNetMobile, NASNetLarge
